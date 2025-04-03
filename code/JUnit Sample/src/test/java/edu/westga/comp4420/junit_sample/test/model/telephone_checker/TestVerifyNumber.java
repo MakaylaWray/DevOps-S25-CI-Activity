d6e28b71bc84be3e73169dcd12fb81a3528892cb
Makayla  Wray <mwray1@my.westga.edu>
2025-04-03 07:20:29 -0400

    Add JUnit Sample Folder to code
    Add Junit Sample Folder to code to hold all source code

---
+++ code/JUnit Sample/src/test/java/edu/westga/comp4420/junit_sample/test/model/telephone_checker/TestVerifyNumber.java
@@ -0,0 +1,36 @@
+package edu.westga.comp4420.junit_sample.test.model.telephone_checker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import edu.westga.comp4420.junit_sample.model.TelephoneChecker;
+
+class TestVerifyNumber {
+
+	@ParameterizedTest
+	@ValueSource(longs = {999999L})
+	void testInvalidNumbers(long number) {
+		TelephoneChecker checker = new TelephoneChecker();
+		
+		boolean result = checker.verifyNumber(number);
+		
+		assertFalse(result, "checking if number was reported as valid");
+	}
+
+	@ParameterizedTest
+	@CsvSource({"999999, false","1000000,true","9999999,true","10000000,false","999999999,false","1000000000,true","9999999999,true","10000000000,false"})
+	void test(long number, boolean expectedResult) {
+		TelephoneChecker checker = new TelephoneChecker();
+		
+		boolean result = checker.verifyNumber(number);
+		
+		assertEquals(expectedResult, result, "checking if number(" + number + ") was reported as valid");
+	}
+	
+}
